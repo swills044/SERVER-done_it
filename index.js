7b115ff55eaa44cb37f7dbc3c91332f70849c8bc
Sean <swills044@gmail.com>
2020-08-07 23:15:43 +0100

    CORS part2

--- index.js
+++ index.js
@@ -30,9 +30,11 @@ var origin = require('./api/origin.js');
 var auth = require('./api/auth.js');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://done-it-server.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+   res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   next();
 });
 
 app.use('/draft', draft);
