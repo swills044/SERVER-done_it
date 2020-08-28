467eac5de80003d7fdc660b583cc7f33b68f4dac
Sean <swills044@gmail.com>
2020-08-28 17:13:58 +0100

    Working on sorting algorithm

--- api/draft.js
+++ api/draft.js
@@ -34,10 +34,140 @@ router.get('/drafts/', async function (req, res) {
     var i = 0;
     //Get all drafts in this db drafts_db_path
     const snapshot = await drafts_db_path.get();
-    console.log(snapshot.docs.length);
-    res.send(snapshot.docs.map(doc => doc.data()))
+    var childs = snapshot.docs.map(doc => doc.data());
+
+    //Check if there are any chileran
+    if (childs.length == 0){
+      res.status(200).send('No drafts here!');
+    } else {
+      sortParentChild(childs);
+    }
+
+    function sortParentChild(childs) {
+      //Make childs go from first child/parent to last child/parent (A hierachy)
+      //Sort by length of the path/id
+
+      //The id will be in 2s
+      var counter = 2;
+      var sortedChilds = [];
+      var sortedLevel = [];
+      var tempArr = [];
+
+      loopChilds();
+      function loopChilds(){
+        for (var i = 0; i < childs.length; i++){
+          //This will check if the array level is matching, starting from the length of 2 (the starting array length because each id has 2 values)
+
+          if (childs[i].path.length === counter){
+            tempArr.push(childs[i]);
+          }
+
+          //This will check if its the end of the array and if so go to the next level of array
+          if (i == childs.length-1){
+            counter = counter + 2;
+
+          }
+
+          //This will check if there is any more childs to loop and if so it will loop the array again
+          if (tempArr.length > 0 && i == childs.length-1){
+            //Then sort this array level by number
+            var sortedLevel = sortArrayLevel(tempArr);
+            tempArr = [];
+            //Add values to the end of the array
+            sortedChilds.push(...sortedLevel);
+            loopChilds();
+          }
+        }
+      }
+
+      //
+      //Helpers
+      //
+      function sortArrayLevel(arr){
+        //Loop array using the
+        arr.sort(function(a,b){
+          //Use only the last two digits for this array level
+          var a1 = a.path.substring(a.path.length-2, a.path.length);
+          var b1 = b.path.substring(b.path.length-2, b.path.length);
+
+          //First remove any underscores
+          var pathA = a1.replace('_', '');
+          var pathB = b1.replace('_', '');
+          //Convert to string
+          //Return comparison
+
+          return Number(pathA) - Number(pathB);
+        })
+        return arr;
+      }
+
+
+      createArrayStructure(sortedChilds);
+
+    }
+
+    function createArrayStructure(childs) {
+      //Loop through hierachy creating arrays to represent the heirachy :
+      //Loop through array structure, each branch should be filled out with the correct ids/paths and those paths should be removed from the branch index so there is never two ids
+
+      //Childs are sorted so we need to create arrays inside every child called childs, which can be filled with the childs below it
+      // Use helper functions for filling the branch with a index of all ids used
+
+      //Two different algo:
+      /*
+      -->go through each child and push their object into the specific array, creating new child arrays when neeeded
+
+      go through each child and fill any childs that belong in its branch of nodes
+
+      */
+
+      var arr = []
+
+      childs.forEach(function(child, i){
+
+        console.log(child.path, child.path.length);
+        var arrLevel = child.path.length;
+        //var index = Number(child.path.replace('_', ''));
+
+        child['childs'] = [];
+        //Need to access the branch by looping throught the correct branches
+        getBranchRef(child.path, childs).push(child);
+
+      })
+
+
+
+    }
+
+    function completeResponse(arr) {
+      res.send(arr);
+    }
+
+    //
+    //Helpers
+    //
+
+    function getBranchRef(path, childs) {
+      var arrLevel = path.length;
+      var tempRef = [];
+      for (var i = 0; i <= arrLevel; i++ ){
+        //Get the index of the path
+        var string_i = path.split(i, i+1);
+        tempRef = childs[]
+
+      }
+    }
+
+    function createBranchIndex() {
+      //object with array of 99 indexes that can be used up
+      return branch_object;
+    }
+
+
+
+
      // draft.id = doc.id;
-     // if (typeof draft.name == 'undefined' || draft.name == ''){
+     // if (typeof draft.path == 'undefined' || draft.path == ''){
      //   self.unallocated.push(draft);
      // }
      // else {
